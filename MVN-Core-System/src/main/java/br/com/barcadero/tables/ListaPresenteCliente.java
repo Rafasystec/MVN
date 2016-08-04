97cc97aea8fe2a8759353b6524438bfdeefb7b4b
antoniorafael <rafasystec@yahoo.com.br>
2016-08-04 14:44:10 -0300

    Alterações feitas na secrel.

--- MVN-Core-System/src/main/java/br/com/barcadero/tables/ListaPresenteCliente.java
+++ MVN-Core-System/src/main/java/br/com/barcadero/tables/ListaPresenteCliente.java
@@ -1,12 +1,13 @@
 package br.com.barcadero.tables;
 
 import java.util.Date;
-
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+import br.com.barcadero.commons.util.HandleDateHour;
 import br.com.barcadero.core.enums.EnumStatusLista;
 import br.com.barcadero.core.util.GlobalNameParam;
 
@@ -14,14 +15,51 @@ import br.com.barcadero.core.util.GlobalNameParam;
 @Table(name="LISTA_PRESENTE_CLIENTES")
 public class ListaPresenteCliente extends EntidadeLoja {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3761482914898199161L;
 	@OneToOne
 	@JoinColumn(name=GlobalNameParam.PARAM_CLIENTE,referencedColumnName=GlobalNameParam.PARAM_DEFAULT_CODE_COLUMN)
 	private Cliente cliente;
-	
-	private String preferencia;
-	private Date validade = null;
+	@Column(name="PREFERENCIA")
+	private String preferencia = "";
+	@Column(name="DT_VALIDADE")
+	private Date validade = HandleDateHour.addYears(new Date(), 1);
+	@Column(name="STATUS_LISTA")
 	private EnumStatusLista status = EnumStatusLista.ABERTA;
+	@Column(name="DESC_LOCAL_EVENTO")
 	private String descLocalEvento = "";
+	public Cliente getCliente() {
+		return cliente;
+	}
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	public String getPreferencia() {
+		return preferencia;
+	}
+	public void setPreferencia(String preferencia) {
+		this.preferencia = preferencia;
+	}
+	public Date getValidade() {
+		return validade;
+	}
+	public void setValidade(Date validade) {
+		this.validade = validade;
+	}
+	public EnumStatusLista getStatus() {
+		return status;
+	}
+	public void setStatus(EnumStatusLista status) {
+		this.status = status;
+	}
+	public String getDescLocalEvento() {
+		return descLocalEvento;
+	}
+	public void setDescLocalEvento(String descLocalEvento) {
+		this.descLocalEvento = descLocalEvento;
+	}
 	
 	
 	
--- MVN-Core-System/src/main/java/br/com/barcadero/tables/ListaPresenteProdCliente.java
+++ MVN-Core-System/src/main/java/br/com/barcadero/tables/ListaPresenteProdCliente.java
@@ -1,10 +1,23 @@
 package br.com.barcadero.tables;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import br.com.barcadero.core.util.GlobalNameParam;
+
 /**
  * Produtos que compoem a lista do cliente
  * @author antoniorafael
  *
  */
+@Entity
+@Table(name="LISTA_PRES_PROD_CLIENTE")
 public class ListaPresenteProdCliente extends EntidadeLoja {
+	
+	@OneToOne
+	@JoinColumn(name=GlobalNameParam.PARAM_PRODUTO, referencedColumnName=GlobalNameParam.PARAM_DEFAULT_CODE_COLUMN)
+	private Produto produto;
 
 }
