4f76dc1925113171cc6d9e343498c73811a4017d
Dailes <dailes@systeconline.com.br>
2016-08-19 22:05:29 -0300

    Alterações para tirar os erros da classe Reste e da pagina de lançamento de contas

--- MVN-Sindata-WEB/src/main/java/br/com/barcadero/web/services/jaxrs/RESTApplication.java
+++ MVN-Sindata-WEB/src/main/java/br/com/barcadero/web/services/jaxrs/RESTApplication.java
@@ -3,8 +3,9 @@ package br.com.barcadero.web.services.jaxrs;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
-
+@ApplicationPath("rest")
 public class RESTApplication extends Application {
 	private Set<Object> singletons = new HashSet<Object>();
 	
