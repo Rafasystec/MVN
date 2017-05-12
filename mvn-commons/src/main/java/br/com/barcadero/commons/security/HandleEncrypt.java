b0a95490903a11b7e42a95b03eb639b115b76574
antoniorafael <rafasystec@yahoo.com.br>
2017-05-11 23:42:51 -0300

    correção cript

--- mvn-commons/src/main/java/br/com/barcadero/commons/security/HandleEncrypt.java
+++ mvn-commons/src/main/java/br/com/barcadero/commons/security/HandleEncrypt.java
@@ -8,50 +8,76 @@ import org.apache.commons.codec.binary.Base64;
 
 public class HandleEncrypt {
 
-	public class Encryptor {
-		
+	private static String key   = "h2R45X7x9r1X34WQ";
+	private static String vetor = "123XTY789HIJ34UU";
+	
+	public static String encrypt(String value) {
+		return encrypt(getKey(), getVetor(), value);
 	}
-	    public static String encrypt(String key, String initVector, String value) {
-	        try {
-	            IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
-	            SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+	
+	public static String decrypt(String value) {
+		return decrypt(getKey(), getVetor(), value);
+	}
+	
+	private static String encrypt(String key, String initVector, String value) {
+		try {
+			IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+			SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
 
-	            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
-	            cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
+			Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+			cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
 
-	            byte[] encrypted = cipher.doFinal(value.getBytes());
-	           
-	            return Base64.encodeBase64String(encrypted);
-	        } catch (Exception ex) {
-	            ex.printStackTrace();
-	        }
+			byte[] encrypted = cipher.doFinal(value.getBytes());
 
-	        return null;
-	    }
+			return Base64.encodeBase64String(encrypted);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 
-	    public static String decrypt(String key, String initVector, String encrypted) {
-	        try {
-	            IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
-	            SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+		return null;
+	}
 
-	            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
-	            cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
+	private static String decrypt(String key, String initVector, String encrypted) {
+		try {
+			IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+			SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
 
-	            byte[] original = cipher.doFinal(Base64.decodeBase64(encrypted));
+			Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+			cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
 
-	            return new String(original);
-	        } catch (Exception ex) {
-	            ex.printStackTrace();
-	        }
+			byte[] original = cipher.doFinal(Base64.decodeBase64(encrypted));
 
-	        return null;
-	    }
+			return new String(original);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 
-	    public static void main(String[] args) {
-	        String key = "Bar12345Bar12345"; // 128 bit key
-	        String initVector = "RandomInitVector"; // 16 bytes IV
+		return null;
+	}
 
-	        System.out.println(decrypt(key, initVector,
-	                encrypt(key, initVector, "Hello World")));
-	    }
+	public static void main(String[] args) {
+		String key = "Bar12345Bar12345"; // 128 bit key
+		String initVector = "RandomInitVector"; // 16 bytes IV
+
+		System.out.println(decrypt(key, initVector,
+				encrypt(key, initVector, "Hello World")));
+	}
+
+	public static String getKey() {
+		return key;
+	}
+
+	public static void setKey(String key) {
+		HandleEncrypt.key = key;
+	}
+
+	public static String getVetor() {
+		return vetor;
+	}
+
+	public static void setVetor(String vetor) {
+		HandleEncrypt.vetor = vetor;
+	}
+
+	
 }
--- mvn-commons/src/test/java/br/com/barcadero/commons/security/TestEncryptor.java
+++ mvn-commons/src/test/java/br/com/barcadero/commons/security/TestEncryptor.java
@@ -5,16 +5,18 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class TestEncryptor {
-	private String key 			= "Bar12345Bar12345"; // 128 bit key
-    private String initVector 	= "RandomInitVector"; // 16 bytes IV
+	public static void main(String[] args) {
+		System.out.println(HandleEncrypt.encrypt("Hello World"));
+	}
+	
 	@Test
     public void testEncrypt() {
-		assertEquals("Criptografia nao confere", "9MU7vSBqfzPnj7iWvvfsEw==", HandleEncrypt.encrypt(key, initVector, "Hello World"));
+		assertEquals("Criptografia nao confere", "aQCJE0l+N9vhzQal52nRVg==", HandleEncrypt.encrypt("Hello World"));
 	}
 	
 	@Test
 	public void testDecrypt() {
-		assertEquals("Descriptografia nao confere", "Hello World", HandleEncrypt.decrypt(key, initVector,"9MU7vSBqfzPnj7iWvvfsEw=="));
+		assertEquals("Descriptografia nao confere", "Hello World", HandleEncrypt.decrypt("aQCJE0l+N9vhzQal52nRVg=="));
 	}
 
 }
